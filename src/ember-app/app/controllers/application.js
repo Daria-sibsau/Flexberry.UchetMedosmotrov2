8485222fe2c56df3c278ea83c11790285ed81bb4
Flexberry-man <mail@flexberry.net>
2023-12-13 11:47:53 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/application.js
+++ src/ember-app/app/controllers/application.js
@@ -28,49 +28,48 @@ export default Controller.extend({
             link: 'i-i-s-uchet-medosmotrov2-сотрудники-l',
             caption: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-сотрудники-l.caption'),
             title: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-сотрудники-l.title'),
-            icon: 'calendar',
+            icon: 'folder open',
             children: null
           }, {
             link: 'i-i-s-uchet-medosmotrov2-подразделения-l',
             caption: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-подразделения-l.caption'),
             title: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-подразделения-l.title'),
-            icon: 'file',
+            icon: 'table',
             children: null
           }, {
             link: 'i-i-s-uchet-medosmotrov2-приказ-м-з-р-l',
             caption: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-приказ-м-з-р-l.caption'),
             title: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-приказ-м-з-р-l.title'),
-            icon: 'address card',
             children: null
           }, {
             link: 'i-i-s-uchet-medosmotrov2-инд-напр-медосм-l',
             caption: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-инд-напр-медосм-l.caption'),
             title: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-инд-напр-медосм-l.title'),
-            icon: 'suitcase',
+            icon: 'archive',
             children: null
           }, {
             link: 'i-i-s-uchet-medosmotrov2-список-на-медосм-l',
             caption: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-список-на-медосм-l.caption'),
             title: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-список-на-медосм-l.title'),
-            icon: 'tags',
+            icon: 'tasks',
             children: null
           }, {
             link: 'i-i-s-uchet-medosmotrov2-организация-l',
             caption: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-организация-l.caption'),
             title: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-организация-l.title'),
-            icon: 'book',
+            icon: 'chart line',
             children: null
           }, {
             link: 'i-i-s-uchet-medosmotrov2-класс-вред-факт-l',
             caption: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-класс-вред-факт-l.caption'),
             title: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-класс-вред-факт-l.title'),
-            icon: 'chart line',
+            icon: 'tags',
             children: null
           }, {
             link: 'i-i-s-uchet-medosmotrov2-должности-l',
             caption: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-должности-l.caption'),
             title: i18n.t('forms.application.sitemap.uchet-medosmotrov2.i-i-s-uchet-medosmotrov2-должности-l.title'),
-            icon: 'tags',
+            icon: 'suitcase',
             children: null
           }]
         }
